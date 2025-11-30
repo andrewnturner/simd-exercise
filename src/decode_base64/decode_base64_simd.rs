f8e8cf0e91d878fc9cf9f5d7cb1ded3e89a285d0
Andrew Turner <andrewturner397@gmail.com>
2025-11-30 17:35:45 +0000

    Use simd in hot loop

--- src/decode_base64/decode_base64_simd.rs
+++ src/decode_base64/decode_base64_simd.rs
@@ -1,3 +1,7 @@
+use std::simd::cmp::SimdPartialEq;
+use std::simd::num::{SimdInt, SimdUint};
+use std::simd::{LaneCount, Simd, SimdElement, SupportedLaneCount};
+
 #[derive(Debug)]
 pub enum DecodeError {
     InvalidByte,
@@ -14,10 +18,10 @@ pub fn decode_base64_simd(data: &[u8], out: &mut Vec<u8>) -> Result<(), DecodeEr
     // Each ascii byte decodes to 6 bits, so 4 ascii bytes decodes to 3 bytes.
     for chunk in data.chunks(4) {
         // Padding with b'A' won't change the value since it maps to 0;
-        let mut ascii = [b'A'; 4];
+        let mut ascii = [b'A'; 8];
         ascii[..chunk.len()].copy_from_slice(chunk);
 
-        let (decoded_bytes, is_ok) = decode_hot(ascii);
+        let (decoded_bytes, is_ok) = decode_hot(ascii.into());
         if !is_ok {
             return Err(DecodeError::InvalidByte);
         }
@@ -36,33 +40,95 @@ pub fn decode_base64_simd(data: &[u8], out: &mut Vec<u8>) -> Result<(), DecodeEr
     Ok(())
 }
 
-fn decode_hot(ascii: [u8; 4]) -> ([u8; 3], bool) {
-    // Buffer for at most 32 bits. We will decode 6 * chunk.len() < 32 bits.
-    let mut decoded_bits = 0u32;
+const fn tiled<T, const N: usize>(tile: &[T]) -> Simd<T, N>
+where
+    T: SimdElement,
+    LaneCount<N>: SupportedLaneCount,
+{
+    let mut out = [tile[0]; N];
 
-    let mut is_ok = true;
-
-    for ascii_byte in ascii {
-        let sextet = match ascii_byte {
-            b'A'..b'Z' => ascii_byte - b'A' + 0,
-            b'a'..b'z' => ascii_byte - b'a' + 26,
-            b'0'..b'9' => ascii_byte - b'0' + 52,
-            b'+' => 62,
-            b'/' => 63,
-            _ => !0,
-        };
-
-        // Shift up our buffer by 6 bits and write the decoded sextet into the space.
-        decoded_bits <<= 6;
-        decoded_bits |= sextet as u32;
-
-        is_ok &= sextet != !0;
+    // Can't use range in const context.
+    let mut i = 0;
+    while i < N {
+        out[i] = tile[i % tile.len()];
+        i += 1;
     }
 
-    // From 4 ascii characters we decode exactly 24 bits of data.
-    let [b3, b2, b1, _] = decoded_bits.to_le_bytes();
+    Simd::from_array(out)
+}
 
-    ([b1, b2, b3], is_ok)
+fn build_selections<const N: usize>() -> Simd<u8, N>
+where
+    LaneCount<N>: SupportedLaneCount,
+{
+    let mut out = [0; N];
+
+    let mut i = 0;
+    while i < N {
+        out[i] = (i + (i / 3)) as u8;
+        i += 1;
+    }
+
+    Simd::from_array(out)
+}
+
+fn decode_hot<const N: usize>(ascii: Simd<u8, N>) -> (Simd<u8, N>, bool)
+where
+    LaneCount<N>: SupportedLaneCount,
+{
+    // This is enough to differentiate the ranges:
+    //   - A..Z == 0x41..0x5b => 4, 5
+    //   - a..z == 0x61..0x7b => 6, 7
+    //   - 0..9 == 0x30..0x3a => 3
+    //   - +    == 0x2b       => 2
+    //   - /    == 0x2f       => 1
+    let hashes = (ascii >> Simd::splat(4))
+        + Simd::simd_eq(ascii, Simd::splat(b'/'))
+            .to_int()
+            .cast::<u8>();
+
+    // Maps the hash to the corresponding offset:
+    //   - A..Z => - b'A' + 0  = -65
+    //   - a..z => - b'a' + 26 = -71
+    //   - 0..9 => - b'0' + 52 = 4
+    //   - +    => - b'+' + 62 = 19
+    //   - /    =? - b'/' + 63 = 16
+    // Value for hash 0 is unused.
+    let offsets_table: [i8; 8] = [0, 16, 19, 4, -65, -65, -71, -71];
+    let tiled_offsets_table = tiled(&offsets_table);
+
+    // Have to use u8 for swizzle_dyn.
+    // With two's complement, positives go to same value while negatives go to 256 + the value.
+    let offsets = tiled_offsets_table.cast::<u8>().swizzle_dyn(hashes);
+
+    // Positive offsets are applied as normal, negative offsets which be 256 too big which overflows
+    // off giving us the correct value.
+    let sextets = ascii + offsets;
+
+    let is_ok = true; // TODO
+
+    // Casting to u16 adds zeros to the left. Then we shift each lane by the correct amount so the
+    // chunks we want to manipulate are at the 8-bit boundaries, and finally can extract the low and
+    // high halves from each. Then we combine the low part with the next lane's high part to pack.
+    //
+    // sextets:   | aaa. | bbb. | ccc. | ddd. |
+    // low_bits:  | .aaa | ..bb | ...c | .... |
+    // high_bits: | .... | b... | cc.. | ddd. |
+    // rotated:   | b... | cc.. | ddd. | .... |
+    // packed:    | baaa | ccbb | dddc | .... |
+    let shifted = sextets.cast::<u16>() << tiled(&[2, 4, 6, 8]);
+    let low_bits = shifted.cast::<u8>();
+    let high_bits = (shifted >> Simd::splat(8)).cast::<u8>();
+
+    let packed = low_bits | high_bits.rotate_elements_left::<1>();
+
+    // Drop every fourth lane.
+    // TODO: ideally would use`simd_swizzle` and `selections` woud be const. But the macro doesn't
+    // seem to work with calls to a const function currently.
+    let selections = build_selections();
+    let output = packed.swizzle_dyn(selections);
+
+    (output, is_ok)
 }
 
 #[cfg(test)]
@@ -80,6 +146,28 @@ mod tests {
         decode_base64_simd(data.as_bytes(), &mut out).unwrap();
 
         let expected = vec![105, 166, 154, 105, 166, 154];
+        assert_eq!(out, expected);
+    }
+
+    #[test]
+    fn test_decode_base64_simd_first_char() {
+        let data = "a";
+
+        let mut out = Vec::new();
+        decode_base64_simd(data.as_bytes(), &mut out).unwrap();
+
+        let expected = vec![104];
+        assert_eq!(out, expected);
+    }
+
+    #[test]
+    fn test_decode_base64_simd_second_char() {
+        let data = "AaA";
+
+        let mut out = Vec::new();
+        decode_base64_simd(data.as_bytes(), &mut out).unwrap();
+
+        let expected = vec![1, 160];
         assert_eq!(out, expected);
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+#![feature(portable_simd)]
+
 pub mod decode_base64;
 pub mod pop_count;
